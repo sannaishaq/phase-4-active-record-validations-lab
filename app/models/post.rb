ccce6d04db5d94a7357ce6028f26c4b52e42e6f8
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-04-26 12:30:00 -0400

    commit tree aecabe06c91d0d02cbe50181011d88102c091b31

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: (['Fiction', 'Non-Fiction']) }
+    validate :must_contain, on: create
+
+
+    def must_contain
+        unless :title.include?("Won't Believe", "Secret",  "Top [number]", "Guess")
+            :title
+        else 
+            nil
+        end
+    end
+
+
 end
+
